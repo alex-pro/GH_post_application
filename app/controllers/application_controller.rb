86db4e43cd59bb08cb0e6532b24c42b894c22626
samsung <a.partitskiy@gmail.com>
2015-11-11 00:23:54 +0200

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :authenticate, except: [:index, :show]
 
-
   def check_user
     redirect_to root_path if logged_in?
   end
